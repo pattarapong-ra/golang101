373734c7a0f095f70ff2c81cf6d0c56e9ff2905e
pattarapong.ra <pattarapong.ra@tnds.com>
2024-07-15 10:57:54 +0700

    [feat] edit main

--- main/main.go
+++ main/main.go
@@ -10,6 +10,27 @@ type Team struct {
 	Amount float64
 }
 
+func main() {
+	// Create an array of team name
+	team := []Team{
+		{"Ham", 5.75},
+		{"Golf", 3.50},
+		{"Parn", 4.50},
+		{"Muiewju", 4.25},
+		{"Petch", 100.00},
+		{"Pack", 5.50},
+	}
+	// Define the goal amount to buy the item
+	var coffeePrice float64 = 15.00
+	fmt.Println("Team Members:", team)
+	fmt.Println("Coffee Price: ", coffeePrice)
+	// code here
+}
+
+// buyCoffee function
+// func buyCoffee() () {
+// }
+
 // for loop
 // Function to calculate the total money in the team
 func calculateTotalMoneyInTeam(team []Team) float64 {
@@ -34,43 +55,4 @@ func checkIfEnoughMoney(total float64, goalAmount float64) bool {
 		fmt.Println("Sorry, you do not have enough money to buy the item.")
 		return false
 	}
-}
-
-func main() {
-	// Create an array of team name
-	team := []Team{
-		{"Ham", 5.75},
-		{"Golf", 3.50},
-		{"Parn", 4.50},
-		{"Muiewju", 4.25},
-		{"Petch", 100.00},
-		{"Pack", 5.50},
-	}
-	// Define the goal amount to buy the item
-	var coffeePrice float64 = 15.00
-	// Calculate the total contributions
-	total := calculateTotalMoneyInTeam(team)
-	// Check if the total contributions are enough to meet the goal amount
-	enoughMoney := checkIfEnoughMoney(total, coffeePrice)
-	// Print the result
-	fmt.Println("Is the total amount enough?", enoughMoney)
-
-	isEnough, change := buyCoffee(total, coffeePrice)
-	if !isEnough {
-		fmt.Println("Not enough money.")
-		return
-	} else {
-		fmt.Printf("Change: $%.2f\n", change)
-	}
-}
-
-func buyCoffee(total float64, price float64) (bool, float64) {
-	var isEnough bool
-	var change float64
-	if total < price {
-		isEnough, change = false, 0
-	} else {
-		isEnough, change = true, total-price
-	}
-	return isEnough, change
 }
